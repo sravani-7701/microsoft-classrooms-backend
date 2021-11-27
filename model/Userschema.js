213197fc57b713bbd5606f19d4ad0d010397be34
Sravani-7701 <sravanishivasln@gmail.com>
2021-11-27 15:17:59 +0530

    jwt correction

--- model/Userschema.js
+++ model/Userschema.js
@@ -57,7 +57,7 @@ UserSchema.pre('save',async function(next) {
 UserSchema.methods.generateAuthToken=async function(){
     try{
       
-     let token=jwt.sign({_id:this._id},MYNAMEISSOMAVARAPUSRAVANISHETTYIAS77010704);
+     let token=jwt.sign({_id:this._id},process.env.SECRET_KEY);
      this.tokens=this.tokens.concat({token:token});
      await this.save();
      return token;
